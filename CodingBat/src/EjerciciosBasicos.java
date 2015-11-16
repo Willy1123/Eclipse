b7792c769b5761d53b9a93fc723af0748fb87e0a
Zeus1123 <guillermoblascoperez@gmail.com>
2015-11-16 19:30:23 +0100

    Creación de la clase EjericiosBasicos
    Primer ejercicio basico

--- CodingBat/src/EjerciciosBasicos.java
+++ CodingBat/src/EjerciciosBasicos.java
@@ -1,4 +1,20 @@
 import acm.program.*;
-public class EjerciciosBasicos {
-
+public class EjerciciosBasicos extends ConsoleProgram{
+	
+	public boolean fiestaArdilla(int bellotas, boolean festivo) {
+		if(festivo) {
+			return true;
+		}
+		
+		if(bellotas >= 40 && bellotas <=60){
+			return true;
+		}
+		return false;
+	}
+	
+	public void run() {
+		println(fiestaArdilla(40, false));
+		println(fiestaArdilla(60, false));
+		println(fiestaArdilla(60, true));
+	}
 }
