5f5a87d1323ec9bb28c32e8038af3c371186546d
Zeus1123 <guillermoblascoperez@gmail.com>
2015-12-03 18:28:09 +0100

    Añadido GameOver, GameWin y Marcador

--- ARKANOID_ACM/src/Arcanoid.java
+++ ARKANOID_ACM/src/Arcanoid.java
@@ -3,6 +3,7 @@ import acm.program.*;
 import acm.util.RandomGenerator;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.*;
@@ -13,7 +14,9 @@ public class Arcanoid extends GraphicsProgram{
 	GImage imagenFondo = new GImage("imagenes/espacio1.jpg");
 	GImage imagenFondo2 = new GImage("imagenes/ECD.gif");
 	GImage fondoJuego = new GImage("Fondo_Juego.jpg");
-	
+	GImage gameOver1 = new GImage("GameOver.png");
+	GImage gameWin = new GImage("GameWin.png");
+
 	// Pantalla.
 	private static int ANCHO_PANTALLA = 1024;
 	private static int ALTO_PANTALLA = 768;
@@ -30,17 +33,27 @@ public class Arcanoid extends GraphicsProgram{
 	GImage cursor = new GImage("cursor.png");
 
 	// Random
-		RandomGenerator aleatorio = new RandomGenerator();
-	
+	RandomGenerator aleatorio = new RandomGenerator();
+
 	// pelota
 	int ancho_pelota = 28;
 	int alto_pelota = 29;
 	GImage pelota = new GImage("pelota.png");
-	double xVelocidad = 3 + aleatorio.nextInt(3); // velocidad de la pelota en el eje x
+	double xVelocidad = 4 + aleatorio.nextInt(5); // velocidad de la pelota en el eje x
 	double yVelocidad = -4 + aleatorio.nextInt(-5); // velocidad de la pelota en el eje y
 
 	
+	//marcador
+	int puntos = 0;
+	GLabel marcador = new GLabel(""+ puntos);
 	
+	//Fuente
+	Font fuente;
+	
+	//uso una variable booleana para indicar que se ha terminado 
+	//la partida
+	boolean gameOver = false;
+
 	public void init(){
 		setSize(ANCHO_PANTALLA, ALTO_PANTALLA);
 
@@ -48,7 +61,7 @@ public class Arcanoid extends GraphicsProgram{
 		GLabel mensaje = new GLabel (" Haz clic con el botón del ratón");
 		add(mensaje, 100, 200);
 		waitForClick();
-		
+
 		//cambio a la segunda imagen porque ya han hecho clic
 		remove (imagenFondo);
 		remove (mensaje);
@@ -57,30 +70,60 @@ public class Arcanoid extends GraphicsProgram{
 		
 		pintaPiramide();
 		
+		//añado el marcador
+		try{
+			fuente = Font.createFont(Font.TRUETYPE_FONT, 
+					getClass().getResourceAsStream("CASTELAR.ttf"));
+		} catch (Exception e) {}
+		marcador.setColor(Color.RED);
+		marcador.setFont(fuente.deriveFont(0, 20));
+		marcador.setLocation(ANCHO_PANTALLA - 200, 50);
+		add(marcador);
+		marcador.setLabel("PUNTOS: "+puntos);
+		
+		
+
 		cursor.setLocation(ANCHO_PANTALLA/2, ALTO_PANTALLA - 150);
 		add(cursor);
 		addMouseListeners();
-		
+
 		//pelota.setLocation(aleatorio.nextInt(ANCHO_PANTALLA/2 + 200), aleatorio.nextInt(ALTO_PANTALLA - 200));
 		add(pelota, ANCHO_PANTALLA/2, ALTO_PANTALLA - 220);
-		
-		
-		
+
+
+
 	}
 
 	public void run() {
-		
-		
 
-		while (true) {
+
+
+		while (!gameOver) {
 			pelota.move(xVelocidad, yVelocidad);
-			cursor.setLocation(pelota.getX() - cursor.getWidth()/2, ALTO_PANTALLA - 150);
+			//cursor.setLocation(pelota.getX() - cursor.getWidth()/2, ALTO_PANTALLA - 150);
 			// Comprobamos si la pelota choca con alguno de los elementos
 			chequeaColision();
+//			GLabel marcador = new GLabel(""+ puntos);
+//			marcador.setColor(Color.RED);
+//			marcador.setFont(fuente.deriveFont(0, 20));
+//			marcador.setLocation(ANCHO_PANTALLA - 100, 50);
+//			add(marcador);
 			// Ponemos una pausa para limitar la velocidad
 			pause(20);
+			
+			if(puntos == 2250){
+				gameOver=true;
+				add(gameWin);
+			}
+
+			if(pelota.getY() > ALTO_PANTALLA) {
+				gameOver = true;
+				add(gameOver1);
+			}
 
 		}
+
+//		add(gameOver1);
 	}
 
 
@@ -142,21 +185,24 @@ public class Arcanoid extends GraphicsProgram{
 	//chequeaCursor devolverá true si ha chocado el cursor con la pelota
 	// y false si no ha chocado.
 	private boolean chequeaCursor(){
-		if (getElementAt(pelota.getX(), pelota.getY()+alto_pelota)==cursor){
-			yVelocidad = -yVelocidad;	
+		if (xVelocidad < 15 && yVelocidad < 15){
+			if (getElementAt(pelota.getX(), pelota.getY()+alto_pelota)==cursor){
+				yVelocidad = -yVelocidad;	
+			}
+			else if (getElementAt(pelota.getX()+alto_pelota, pelota.getY()+alto_pelota)==cursor){
+				yVelocidad = -yVelocidad;	
+			}
+			else if (getElementAt(pelota.getX(), pelota.getY())==cursor){
+				xVelocidad = -xVelocidad;	
+			}
+			else if (getElementAt(pelota.getX()+alto_pelota, pelota.getY())==cursor){
+				xVelocidad = -xVelocidad;	
+			}
+
+			else {
+				return false;
+			} 
 		}
-		else if (getElementAt(pelota.getX()+alto_pelota, pelota.getY()+alto_pelota)==cursor){
-			yVelocidad = -yVelocidad;	
-		}
-		else if (getElementAt(pelota.getX(), pelota.getY())==cursor){
-			xVelocidad = -xVelocidad;	
-		}
-		else if (getElementAt(pelota.getX()+alto_pelota, pelota.getY())==cursor){
-			xVelocidad = -xVelocidad;	
-		}
-		else {
-			return false;
-		} 
 		return true;
 	}
 
@@ -201,8 +247,12 @@ public class Arcanoid extends GraphicsProgram{
 		auxiliar = getElementAt(posX, posY);
 
 		// Chequeamos los ladrillos
-		if ((auxiliar != cursor) && (auxiliar != fondoJuego) && (auxiliar != pelota)) {
+		if ((auxiliar != cursor) && (auxiliar != fondoJuego) && (auxiliar != pelota) && (auxiliar != null) && (auxiliar != marcador) ) {
 			remove(auxiliar);
+			puntos = puntos + 50;
+			marcador.setLabel("PUNTOS: "+puntos);
+			
+//			remove(marcador);
 			if (direccion == 'y') {
 				yVelocidad = -yVelocidad;
 			} else {
